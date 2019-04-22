a39b6aa327d115dbb0626c64e2211f67c88f0648
Ruben Hur <2001.hcy@gmail.com>
2019-04-22 15:06:22 +0900

    init

---
+++ src/main/java/kr/hs/dgsw/userpostcrud/UserpostcrudApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.userpostcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserpostcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserpostcrudApplication.class, args);
+    }
+
+}
